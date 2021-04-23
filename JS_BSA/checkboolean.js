9a67f01782e9f01f294111bfaeebcd5ee37cbe81
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-04-23 22:00:04 +0530

    Update checkboolean.js
    Falsy way of javascript

--- JS_BSA/checkboolean.js
+++ JS_BSA/checkboolean.js
@@ -11,5 +11,15 @@ Solution
 function bouncer(arr) {
   return arr.filter(Boolean);
 }
+/*Alternate solution - flasy way
+let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+  //Before pushing the array to the new array - we're checking, if data is valid or not- if valid that pushing it 
+    if (arr[i]) {
+      newArray.push(arr[i]);
+      }
+  }
+  return newArray;
+*/
 
 bouncer([7, "ate", "", false, 9]);
