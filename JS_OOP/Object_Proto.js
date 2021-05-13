fb0520dd75261d76848a7812a27e6b9c1bc32973
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-05-13 18:56:08 +0530

    Add files via upload
    Checking function prototype of object is true || False

---
+++ JS_OOP/Object_Proto.js
@@ -0,0 +1,20 @@
+/*
+Understand Where an Object’s Prototype Comes From
+
+Just like people inherit genes from their parents, an object inherits its prototype directly from the constructor function that created it. For example, here the Bird constructor creates the duck object:
+
+function Bird(name) {
+  this.name = name;
+}
+
+let duck = new Bird("Donald");
+
+duck inherits its prototype from the Bird constructor function. You can show this relationship with the isPrototypeOf method:
+
+Bird.prototype.isPrototypeOf(duck);
+
+This would return true.
+
+Use isPrototypeOf to check the prototype of beagle.
+*/
+Solution
