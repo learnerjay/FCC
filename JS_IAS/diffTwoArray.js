88c338994eaa361632e1d6fd9a9b2b7c8d0f4141
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-07-01 22:49:20 +0530

    Update diffTwoArray.js
    3rd solution

--- JS_IAS/diffTwoArray.js
+++ JS_IAS/diffTwoArray.js
@@ -32,4 +32,21 @@ var newArr = [];
     }
   }
     return newArr;
+3.
+var newArr = [];
+
+  function onlyInFirst(first, second) {
+    // Looping through an array to find elements that don't exist in another array
+    for (var i = 0; i < first.length; i++) {
+      if (second.indexOf(first[i]) === -1) {
+        // Pushing the elements unique to first to newArr
+        newArr.push(first[i]);
+      }
+    }
+  }
+
+  onlyInFirst(arr1, arr2);
+  onlyInFirst(arr2, arr1);
+
+  return newArr;
 */
