5a8191ee3721fe684bda9cc01787db61e5cd425d
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-07-10 23:28:17 +0530

    Update iife_Module.js
    ES6 way

--- JS_OOP/iife_Module.js
+++ JS_OOP/iife_Module.js
@@ -53,3 +53,16 @@ let funModule = (function () {
     }
   }
 })();
+/*
+let funModule = ( () => {
+  return {
+    isCuteMixin: (obj) => {
+      obj.isCute = () => { true; };
+    },
+    singMixin: (obj) => {
+      obj.sing = () => { console.log("Singing to an awesome tune"); }
+    }
+
+  }
+})();
+*/
