bcfee9e9afa2cca3a2977bffbb46e36c132660b8
Jayant Kumar <45159010+learnerjay@users.noreply.github.com>
2021-09-24 23:56:09 +0530

    Update sumPrime.js
    Another solution

--- JS_IAS/sumPrime.js
+++ JS_IAS/sumPrime.js
@@ -26,3 +26,11 @@ function sumPrimes(num) {
   }
 
 sumPrimes(10);
+/*
+let primes = [];
+  for (let i = 2; i <= num; i++) {
+    if (primes.every((prime) => i % prime !== 0))
+      primes.push(i);
+  }
+  return primes.reduce((sum, prime) => sum + prime, 0);
+*/
